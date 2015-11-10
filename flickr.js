ba98f03ec2859c281b12f77afe55ca3d605c6fea
lance9980 <lance9980@gmail.com>
2015-11-10 15:44:18 -0800

    flickr v.2 modal test

--- flickr.js
+++ flickr.js
@@ -1,28 +1,40 @@
 //Flickr related code only!
 
+
 $(document).ready(function() {
-   // $('.btn-info').on('click', (clearBox));
-    $('.btn-info').on('click', (getFlickr));
+    $('.btn-info').on('click', function() {
+        clearBox();
+        getFlickr();
+    });
+    $('#flicker-section img').on('click', function() {
+        popupImage();
+    });
 });
 
-// Get Flickr pictures via API call
-function getFlickr() {
-    var flickrData;
-    var keyWord = $("input").val();
+var flickrData;
 
+// Get Flickr photos via API call and append to #flicker-section DOM
+function getFlickr() {
+
+    var keyWord = $("input").val();
     apis.flickr.getData(keyWord, 5, function (success, resp) {
         if(success) {
             $("#flicker-section").append("<br>");
             flickrData = resp;
+            //remove below when done
             console.log("Flickr:", success, resp);
 
+            // Loop to append images to #flicker-section div
             for (var i=0; i<flickrData.photos.photo.length; i++) {
                 var img = $("<img>", {
-                    src: resp.urls[i],
-                    height: "100",
-                    width: "100"
+                    src: flickrData.urls[i]
                 });
-                $("#flicker-section").append(img);
+                var linkImg = $("<a>", {
+                    href: flickrData.urls[i]
+                    //target: "_blank"
+                }).attr("data-toggle","modal");
+                linkImg.append(img);
+                $("#flicker-section").append(linkImg);
             }
         }
         else {
@@ -31,6 +43,12 @@ function getFlickr() {
     });
 }
 
+// Clear DOM contents prior to populating data
 function clearBox() {
-    $("#flicker-section").replaceWith("");
+    $("#flicker-section").empty();
+    $("#flicker-section").text("Flickr");
+}
+
+function popupImage() {
+
 }
