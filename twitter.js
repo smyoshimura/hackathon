fde5d01bcc1020111ae26d2b19b0af3683716187
David Goodman <deg5112@gmail.com>
2015-11-10 11:41:51 -0800

    update to html and twitter doc

--- twitter.js
+++ twitter.js
@@ -1,1 +1,36 @@
+
 //Twitter related code only!
+//
+//twitter: {
+//    getData: function(search, callback){
+//        this.errors = [];
+//
+//        if(typeof search != "string"){
+//            this.errors.push("'Search' must be a string, you gave a " + typeof search);
+//        }
+//
+//        if(typeof callback != "undefined" && typeof callback != "function"){
+//            this.errors.push("'Callback' must be a function, you gave a " + typeof callback);
+//        }
+//
+//        if(this.errors.length > 0){
+//            console.log("There were errors with your request:", this.errors);
+//            return this.errors;
+//        }else {
+//            var data = {search_term: search};
+//            var url = "http://s-apis.learningfuze.com/hackathon/twitter/index.php";
+//            apis.ajax(data, url, callback);
+//        }
+//    },
+//    errors: []
+//
+//},
+function twitterCallback(response){
+    console.log(response);
+}
+$(function(){
+    $('button').click(function(){
+        var search = $('input').val();
+        apis.twitter.getData(search, twitterCallback);
+    });
+});
