84c29d0ec13f75843ed62e62e6fc330da035cbe7
mihirshahgithub <mshah1086@gmail.com>
2015-11-11 10:26:38 -0800

    updated comments

--- youtube.js
+++ youtube.js
@@ -1,8 +1,8 @@
 //pulling video from youtube and playing
 $(document).ready(function () {
     $('#search-button').click(function () {
-        var searchInput = $('input').val();  //storing the input value in the search bar into searchInput variable
-        console.log(searchInput);  //checking to see if input is stored properly
+        var searchInput = $('input').val();                                                                //storing the input value in the search bar into searchInput variable
+        console.log(searchInput);                                                                          //checking to see if input is stored properly
         apis.youtube.getData(searchInput, 2, function (success, response) {                                //using created getData function with searchInput, 2, and a callback function as parameters
             if (success) {                                                                                 //if data is retrieved and stored, call the playVideo function; currently, only accesses index 0
                 console.log(response);
@@ -11,7 +11,7 @@ $(document).ready(function () {
                     apis.youtube.stopVideo()
                 }, 40000);
             } else {
-                console.log("YouTube Failed");                                                           //if success is false, console will notify failure
+                console.log("YouTube Failed");                                                             //if success is false, console will notify failure
             }
         });
     })
