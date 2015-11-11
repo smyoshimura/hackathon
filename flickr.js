d20438846c588e44ab21890609a20190abddc9eb
lance9980 <lance9980@gmail.com>
2015-11-10 16:39:12 -0800

    flickr v.2 alink popup

--- flickr.js
+++ flickr.js
@@ -1,28 +1,24 @@
 //Flickr related code only!
 
-
 $(document).ready(function() {
-    $('.btn-info').on('click', function() {
+    $('#search-button').on('click', function() {
         clearBox();
         getFlickr();
     });
-    $('#flicker-section img').on('click', function() {
-        popupImage();
-    });
 });
 
-var flickrData;
+
 
 // Get Flickr photos via API call and append to #flicker-section DOM
 function getFlickr() {
-
+    var flickrData;
     var keyWord = $("input").val();
-    apis.flickr.getData(keyWord, 5, function (success, resp) {
+    var numPhotos = 9;
+
+    apis.flickr.getData(keyWord, numPhotos, function (success, resp) {
         if(success) {
-            $("#flicker-section").append("<br>");
+            $("#flickr-section").append("<br>");
             flickrData = resp;
-            //remove below when done
-            console.log("Flickr:", success, resp);
 
             // Loop to append images to #flicker-section div
             for (var i=0; i<flickrData.photos.photo.length; i++) {
@@ -30,11 +26,12 @@ function getFlickr() {
                     src: flickrData.urls[i]
                 });
                 var linkImg = $("<a>", {
-                    href: flickrData.urls[i]
-                    //target: "_blank"
-                }).attr("data-toggle","modal");
+                    href: flickrData.urls[i],
+                    target: "_blank"
+                }).attr("data-toggle","modal").attr("data-target","#flickr-modal");
+
                 linkImg.append(img);
-                $("#flicker-section").append(linkImg);
+                $("#flickr-section").append(linkImg);
             }
         }
         else {
@@ -45,10 +42,6 @@ function getFlickr() {
 
 // Clear DOM contents prior to populating data
 function clearBox() {
-    $("#flicker-section").empty();
-    $("#flicker-section").text("Flickr");
-}
-
-function popupImage() {
-
+    $("#flickr-section").empty();
+    $("#flickr-section").text("Flickr");
 }
