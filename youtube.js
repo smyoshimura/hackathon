9f8ad0e4ee15da5c5a977de5b97a6e8d8b58e26c
mihirshahgithub <mshah1086@gmail.com>
2015-11-10 16:21:23 -0800

    STUPID SEARCH DOUBLE INPUT, AHH

--- youtube.js
+++ youtube.js
@@ -1,21 +1,36 @@
 //pulling video from youtube and playing
 $(document).ready(function () {
     $('.btn').click(function () {
-        //console.log("clicked");
+        if(document.getElementsByTagName('iframe')){
+            $('iframe').replaceWith('<div id="player"></div>');
+        }
         var searchInput=$('input').val();
         console.log(searchInput);
-        apis.youtube.getData(searchInput,2, function(success, response){
+        apis.youtube.getData(searchInput,5, function(success, response){
             if(success) {
                 console.log(response);
-                apis.youtube.playVideo(response.video[0].id, "590", 520);
-                setTimeout(function () {
+                apis.youtube.playVideo(response.video[0].id);
+               setTimeout(function () {
                     apis.youtube.stopVideo()
                 }, 20000);
+                //$('#player').replaceWith();
             }else{
                 console.log("YouTube Failed");
             }
         });
-    })
+    });
 });
 
 //console.log(apis.youtube.playerInfo.player);
+
+//need to find a way to get the input to register when a video is alredy present
+//dom create a button to switch videos
+
+var deleteB = $('<button>', {
+    type: "button",
+    class: "btn btn-info",
+    text: "Next"
+});
+
+var test=$('.row').append(deleteB);
+
