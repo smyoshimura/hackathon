28f05350afd7dda0a0ea3b3764449a0bba6944f5
lance9980 <lance9980@gmail.com>
2015-11-10 17:58:12 -0800

    flickr modal test

--- flickr.js
+++ flickr.js
@@ -8,40 +8,51 @@ $(document).ready(function() {
 });
 
 
-
 // Get Flickr photos via API call and append to #flicker-section DOM
 function getFlickr() {
     var flickrData;
     var keyWord = $("input").val();
-    var numPhotos = 9;
+    var numPhotos = 5;
 
-    apis.flickr.getData(keyWord, numPhotos, function (success, resp) {
-        if(success) {
-            $("#flickr-section").append("<br>");
-            flickrData = resp;
+    if (keyWord !== "") {
+        apis.flickr.getData(keyWord, numPhotos, function (success, resp) {
+            if (success) {
+                $("#flickr-section").append("<br>");
+                flickrData = resp;
 
-            // Loop to append images to #flicker-section div
-            for (var i=0; i<flickrData.photos.photo.length; i++) {
-                var img = $("<img>", {
-                    src: flickrData.urls[i]
-                });
-                var linkImg = $("<a>", {
-                    href: flickrData.urls[i],
-                    target: "_blank"
-                }).attr("data-toggle","modal").attr("data-target","#flickr-modal");
+                // Loop to append images to #flicker-section div
+                for (var i = 0; i < flickrData.photos.photo.length; i++) {
+                    var img = $("<img>", {
+                        src: flickrData.urls[i]
+                    });
+                    //var linkImg = $("<a>", {
+                    //    href: flickrData.urls[i],
+                    //    target: "_blank"
+                    //}).attr("data-toggle", "modal").attr("data-target", "#flickr-modal");
+                    //
+                    //linkImg.append(img);
+                    //$("#flickr-section").append(linkImg);
 
-                linkImg.append(img);
-                $("#flickr-section").append(linkImg);
+                    var linkA = $("<a>").attr("href","#flickr-modal").attr("data-toggle", "modal");
+                    var modalA = linkA.append(img);
+                    var modalList = $("<li>").append(modalA);
+                    $("ul.nav").append(modalList);
+                    console.log(modalList);
+
+                }
             }
-        }
-        else {
-            console.log("Flickr failed");
-        }
-    });
+            else {
+                console.log("Flickr failed");
+            }
+        });
+    }
+    else {
+        $("#flickr-section").text("Nothing entered. Search something!");
+    }
 }
 
 // Clear DOM contents prior to populating data
 function clearBox() {
-    $("#flickr-section").empty();
-    $("#flickr-section").text("Flickr");
+    $("ul.nav").empty();
+   // $("#flickr-section").text("Flickr");
 }
