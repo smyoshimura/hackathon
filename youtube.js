cd3cdb167972aca0bb6284a64fc20418eef56c38
David Goodman <deg5112@gmail.com>
2015-11-12 11:11:59 -0800

    fixed flicker style

--- youtube.js
+++ youtube.js
@@ -1,23 +1,23 @@
+//TODO make a category for itunes api, and based off the search results.. whatever you click, another ajax request goes down and then your modal pops ups
 //pulling video from youtube and playing
 $(document).ready(function () {
-
     $('#search-button').click(function () {
-        //console.log("clicked");
-        var searchInput=$('input').val();
-        console.log(searchInput);
-        apis.youtube.getData(searchInput,3, function(success, response){
-            if(success) {
-                console.log(response);
-                youtubeArray = response.video;
-                apis.youtube.playVideo(response.video[0].id, '55%', '60%');
-                setTimeout(function () {
-                    apis.youtube.stopVideo()
-                }, 20000);
-            }else{
-                console.log("YouTube Failed");
-            }
-        });
+        if (document.getElementsByTagName('iframe')) {
+            $('iframe').replaceWith('<div id="player"></div>');
+            var searchInput = $('input').val();                                                                //storing the input value in the search bar into searchInput variable
+            console.log(searchInput);                                                                          //checking to see if input is stored properly
+            apis.youtube.getData(searchInput, 2, function (success, response) {                                //using created getData function with searchInput, 2, and a callback function as parameters
+                if (success) {                                                                                 //if data is retrieved and stored, call the playVideo function; currently, only accesses index 0
+                    console.log(response);
+                    apis.youtube.playVideo(response.video[0].id, 300, 450);
+                    setTimeout(function () {                                                                   //using a setTimeout function and calling stopVideo() at 40 seconds
+                        apis.youtube.stopVideo()
+                    }, 40000);
+                    onYouTubeIframeAPIReady();
+                } else {
+                    console.log("YouTube Failed");                                                             //if success is false, console will notify failure
+                }
+            });
+        }
     });
 });
-
-//console.log(apis.youtube.playerInfo.player);
