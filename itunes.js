4f5109d11cd84d94485bc68e405e0adc3a68b675
David Goodman <deg5112@gmail.com>
2015-11-12 17:22:42 -0800

    itunes search val almost done

--- itunes.js
+++ itunes.js
@@ -1,31 +1,50 @@
+var itunesArray;
+var searchVal;
 $(function(){
+    //select an genere, movies pop up
     $('select').change(function(){//on select of genre,, loop through i-array and grab the code, stick in url and run the itunes get function
-        var itunesArray = [{genre: 'Comedy', code: 4404}, {genre: 'Action & Adventure', code: 4401}];
+        var selectArray = [{genre: 'Comedy', code: 4404}, {genre: 'Action & Adventure', code: 4401}];
         var curGenre = $('.option:selected').text();
         var curCode;
         var url;
-        for(var x = 0; x<itunesArray.length; x++){
-            if(itunesArray[x].genre === curGenre){
-                curCode = itunesArray[x].code;
+        for(var x = 0; x<selectArray.length; x++){
+            if(selectArray[x].genre === curGenre){
+                curCode = selectArray[x].code;
                 url = 'https://itunes.apple.com/us/rss/topmovies/limit=10/genre=' + curCode + '/json';
             }
         }
         getFromItunes(url);
     });
+
 });
 
 function getFromItunes(url){
+    //make image element and give it a click handler to find the the search value for youtube, twitter, and flicker
     var img;
-    var link;
-    var col;
     $.ajax({
         dataType: 'json',
         url: url,
         success: function(result) {
+            itunesArray = result.feed.entry;
+
+            //loop
+
             for(var i =0; i<result.feed.entry.length;i++){
                 col = $('<div>').addClass('col-xs-2');
                 link = result.feed.entry[i]['im:image'][2].label;
-                img = $('<img>').attr('src', link);
+                img = $('<img>').attr('src', link).addClass('movie').click(function() {
+
+                    var curSrc = $(this).attr('src');
+
+                    for (var y = 0; y < itunesArray.length; y++) {
+                        if (curSrc === itunesArray[y]['im:image'][2]) {
+                            searchVal = itunesArray[y]['im:name'].label;
+
+                        }
+                    }
+                });
+
+
                 col.append(img);
                 $('#itunes-results').append(col);
             }
